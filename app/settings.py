831991a431b9c2c5c01fa9d4fc31b16388858540
miko <islamimanhod@gmail.com>
2022-07-28 23:02:28 +0600

    feat configure db

--- app/settings.py
+++ app/settings.py
@@ -1,9 +1,21 @@
+from typing import Callable
 from pydantic import BaseSettings
-from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import Session, sessionmaker
 
 class Settings(BaseSettings):
-    db_url: str
+    database_url: str
 
+    class Config:
+        env_file = ".env"
 
-settings = Settings()
-async_engine = create_async_engine(settings.db_address, future=True)
+    
+
+def get_db_session()-> Callable[..., Session]:
+    settings = Settings()
+    async_engine = create_async_engine(settings.database_url, future=True, echo=True)
+    return sessionmaker(
+        async_engine,
+        expire_on_commit=False,
+        class_=AsyncSession
+    )
